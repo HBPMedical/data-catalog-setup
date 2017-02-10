172c66bdf14aca649d6aba8acdd637073cda8c84
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-10 16:42:51 +0100

    big db update

--- data_catalog_schema.py
+++ data_catalog_schema.py
@@ -9,18 +9,6 @@ Base = declarative_base()
 metadata = Base.metadata
 
 
-class Check(Base):
-    __tablename__ = 'check'
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    quality_check_id = Column(ForeignKey('quality_check.id'), nullable=False, index=True)
-    data_file_id = Column(ForeignKey('data_file.id'), nullable=False, index=True)
-    value = Column(Float, nullable=False)
-
-    data_file = relationship('DataFile')
-    quality_check = relationship('QualityCheck')
-
-
 class Provenance(Base):
     __tablename__ = 'provenance'
 
@@ -51,7 +39,7 @@ class DataFile(Base):
     __tablename__ = 'data_file'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    repetition_id = Column(ForeignKey('repetition.id'), nullable=False, index=True)
+    repetition_id = Column(ForeignKey('repetition.id'), nullable=True, index=True)
     processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
     path = Column(Text, nullable=False)
     type = Column(Text, nullable=False)
@@ -63,77 +51,16 @@ class DataFile(Base):
     processing_step = relationship('ProcessingStep')
 
 
-class Participant(Base):
-    __tablename__ = 'participant'
-
-    id = Column(String(255), primary_key=True)
-    gender = Column(Enum('male', 'female', 'other', 'unknown', name='gender'), nullable=False)
-    handedness = Column(Enum('left', 'right', 'ambidexter', 'unknown', name='handedness'), nullable=False)
-    birthdate = Column(Date, nullable=True)
-    age = Column(Float, nullable=True)
-
-
-class Project(Base):
-    __tablename__ = 'project'
-
-    id = Column(Integer, primary_key=True)
-    researcher_id = Column(ForeignKey('researcher.id'), nullable=False, index=True)
-    scan_id = Column(ForeignKey('scan.id'), nullable=False, index=True)
-    name = Column(String(255), nullable=False, unique=True)
-
-    researcher = relationship('Researcher')
-    scan = relationship('Scan')
-
-
-class QualityCheck(Base):
-    __tablename__ = 'quality_check'
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    scientist_id = Column(ForeignKey('researcher.id'), nullable=False, index=True)
-
-    scientist = relationship('Researcher')
-
-
 class Repetition(Base):
     __tablename__ = 'repetition'
 
     id = Column(Integer, primary_key=True, nullable=False)
     sequence_id = Column(ForeignKey('sequence.id'), nullable=False, index=True)
     value = Column(Integer, nullable=False)
+    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
 
     sequence = relationship('Sequence')
-
-
-class Researcher(Base):
-    __tablename__ = 'researcher'
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(255), nullable=False)
-    firstname = Column(String(255), nullable=False)
-    lastname = Column(String(255), nullable=False)
-
-
-class Responsible(Base):
-    __tablename__ = 'responsible'
-
-    scientist_id = Column(ForeignKey('researcher.id'), primary_key=True, nullable=False, index=True)
-    scan_id = Column(ForeignKey('scan.id'), primary_key=True, nullable=False, index=True)
-    role = Column(Enum('technician', 'supervisor', name='responsible_role'), nullable=False)
-
-    scan = relationship('Scan')
-    scientist = relationship('Researcher')
-
-
-class Scan(Base):
-    __tablename__ = 'scan'
-
-    id = Column(Integer, primary_key=True)
-    date = Column(Date, nullable=False)
-    role = Column(Enum('C', 'P', 'IC', 'U', name='scan_role'), nullable=False)
-    comment = Column(Text, nullable=False)
-    participant_id = Column(ForeignKey('participant.id'), nullable=False, index=True)
-
-    participant = relationship('Participant')
+    processing_step = relationship('ProcessingStep')
 
 
 class Sequence(Base):
@@ -142,9 +69,11 @@ class Sequence(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     session_id = Column(ForeignKey('session.id'), nullable=False, index=True)
     sequence_type_id = Column(ForeignKey('sequence_type.id'), nullable=False, index=True)
+    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
 
     sequence_type = relationship('SequenceType')
     session = relationship('Session')
+    processing_step = relationship('ProcessingStep')
 
 
 class SequenceType(Base):
@@ -173,11 +102,40 @@ class SequenceType(Base):
     pixel_spacing_1 = Column(Float, nullable=True)
 
 
+class Scan(Base):
+    __tablename__ = 'scan'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    role = Column(Enum('C', 'P', 'IC', 'U', name='scan_role'), nullable=False)
+    comment = Column(Text, nullable=False)
+    participant_id = Column(ForeignKey('participant.id'), nullable=False, index=True)
+    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
+
+    participant = relationship('Participant')
+    processing_step = relationship('ProcessingStep')
+
+
 class Session(Base):
     __tablename__ = 'session'
 
     id = Column(Integer, primary_key=True, nullable=False)
     scan_id = Column(ForeignKey('scan.id'), nullable=False, index=True)
     value = Column(String, nullable=False)
+    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
 
     scan = relationship('Scan')
+    processing_step = relationship('ProcessingStep')
+
+
+class Participant(Base):
+    __tablename__ = 'participant'
+
+    id = Column(String(255), primary_key=True)
+    gender = Column(Enum('male', 'female', 'other', 'unknown', name='gender'), nullable=False)
+    handedness = Column(Enum('left', 'right', 'ambidexter', 'unknown', name='handedness'), nullable=False)
+    birthdate = Column(Date, nullable=True)
+    age = Column(Float, nullable=True)
+    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
+
+    processing_step = relationship('ProcessingStep')
