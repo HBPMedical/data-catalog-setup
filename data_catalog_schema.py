3156f70363ef40ed26cf26a9b600a879b1c68aad
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-13 11:53:29 +0100

    new schema

--- data_catalog_schema.py
+++ data_catalog_schema.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from sqlalchemy import Boolean
-from sqlalchemy import Column, Date, Enum, Float, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, Index, ForeignKey, Enum, DATE, DECIMAL, INTEGER, VARCHAR, TEXT
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,24 +12,24 @@ metadata = Base.metadata
 class Provenance(Base):
     __tablename__ = 'provenance'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    dataset = Column(Text, nullable=False)
-    matlab_version = Column(Text, nullable=True)
-    spm_version = Column(Text, nullable=True)
-    spm_revision = Column(Text, nullable=True)
-    fn_called = Column(Text, nullable=True)
-    fn_version = Column(Text, nullable=True)
-    others = Column(Text, nullable=True)
+    id = Column(INTEGER, primary_key=True)
+    dataset = Column(VARCHAR(50), nullable=False)
+    matlab_version = Column(VARCHAR(50))
+    spm_version = Column(VARCHAR(50))
+    spm_revision = Column(VARCHAR(50))
+    fn_called = Column(VARCHAR(50))
+    fn_version = Column(VARCHAR(50))
+    others = Column(TEXT)
 
 
 class ProcessingStep(Base):
     __tablename__ = 'processing_step'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    previous_step_id = Column(ForeignKey('processing_step.id'), nullable=True, index=True)
-    provenance_id = Column(ForeignKey('provenance.id'), nullable=False, index=True)
-    name = Column(Text, nullable=False)
-    execution_date = Column(Date, nullable=True)
+    id = Column(INTEGER, primary_key=True)
+    previous_step_id = Column(ForeignKey('processing_step.id'))
+    provenance_id = Column(ForeignKey('provenance.id'), nullable=False)
+    name = Column(VARCHAR(200), nullable=False)
+    execution_date = Column(DATE)
 
     processing_step = relationship('ProcessingStep')
     provenance = relationship('Provenance')
@@ -38,14 +38,14 @@ class ProcessingStep(Base):
 class DataFile(Base):
     __tablename__ = 'data_file'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    repetition_id = Column(ForeignKey('repetition.id'), nullable=True, index=True)
-    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
-    path = Column(Text, nullable=False)
-    type = Column(Text, nullable=False)
-    result_type = Column(String(255), nullable=True)
-    output_type = Column(String(255), nullable=True)
-    is_copy = Column(Boolean, nullable=True)
+    id = Column(INTEGER, primary_key=True)
+    repetition_id = Column(ForeignKey('repetition.index_id'))
+    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False)
+    path = Column(TEXT, unique=True, nullable=False)
+    type = Column(VARCHAR(50), nullable=False)
+    result_type = Column(VARCHAR(50))
+    output_type = Column(VARCHAR(50))
+    is_copy = Column(Boolean)
 
     repetition = relationship('Repetition')
     processing_step = relationship('ProcessingStep')
@@ -54,88 +54,87 @@ class DataFile(Base):
 class Repetition(Base):
     __tablename__ = 'repetition'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    sequence_id = Column(ForeignKey('sequence.id'), nullable=False, index=True)
-    value = Column(Integer, nullable=False)
-    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
+    name = Column(VARCHAR(50), primary_key=True)
+    sequence_id = Column(ForeignKey('sequence.index_id'), primary_key=True)
+    date = Column(DATE)
+    index_id = Column(INTEGER, nullable=False, autoincrement=True, index=True, unique=True)
 
     sequence = relationship('Sequence')
-    processing_step = relationship('ProcessingStep')
 
 
 class Sequence(Base):
     __tablename__ = 'sequence'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    session_id = Column(ForeignKey('session.id'), nullable=False, index=True)
-    sequence_type_id = Column(ForeignKey('sequence_type.id'), nullable=False, index=True)
-    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
+    name = Column(VARCHAR(50), primary_key=True)
+    session_id = Column(ForeignKey('session.index_id'), primary_key=True)
+    sequence_type_id = Column(ForeignKey('sequence_type.id'), nullable=False)
+    index_id = Column(INTEGER, nullable=False, autoincrement=True, index=True, unique=True)
 
     sequence_type = relationship('SequenceType')
     session = relationship('Session')
-    processing_step = relationship('ProcessingStep')
 
 
 class SequenceType(Base):
     __tablename__ = 'sequence_type'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    manufacturer = Column(String(255), nullable=True)
-    manufacturer_model_name = Column(String(255), nullable=True)
-    institution_name = Column(String(255), nullable=True)
-    slice_thickness = Column(Float, nullable=True)
-    repetition_time = Column(Float, nullable=True)
-    echo_time = Column(Float, nullable=True)
-    echo_number = Column(Integer, nullable=True)
-    number_of_phase_encoding_steps = Column(Integer, nullable=True)
-    percent_phase_field_of_view = Column(Float, nullable=True)
-    pixel_bandwidth = Column(Integer, nullable=True)
-    flip_angle = Column(Float, nullable=True)
-    rows = Column(Integer, nullable=True)
-    columns = Column(Integer, nullable=True)
-    magnetic_field_strength = Column(Float, nullable=True)
-    space_between_slices = Column(Float, nullable=True)
-    echo_train_length = Column(Integer, nullable=True)
-    percent_sampling = Column(Float, nullable=True)
-    pixel_spacing_0 = Column(Float, nullable=True)
-    pixel_spacing_1 = Column(Float, nullable=True)
-
-
-class Scan(Base):
-    __tablename__ = 'scan'
-
-    id = Column(Integer, primary_key=True)
-    date = Column(Date, nullable=False)
-    role = Column(Enum('C', 'P', 'IC', 'U', name='scan_role'), nullable=False)
-    comment = Column(Text, nullable=False)
-    participant_id = Column(ForeignKey('participant.id'), nullable=False, index=True)
-    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
-
-    participant = relationship('Participant')
-    processing_step = relationship('ProcessingStep')
+    id = Column(INTEGER, primary_key=True)
+    name = Column(VARCHAR(200), nullable=False)
+    manufacturer = Column(VARCHAR(200), nullable=True)
+    manufacturer_model_name = Column(VARCHAR(200), nullable=True)
+    institution_name = Column(VARCHAR(200), nullable=True)
+    slice_thickness = Column(DECIMAL, nullable=True)
+    repetition_time = Column(DECIMAL, nullable=True)
+    echo_time = Column(DECIMAL, nullable=True)
+    echo_number = Column(INTEGER, nullable=True)
+    number_of_phase_encoding_steps = Column(INTEGER, nullable=True)
+    percent_phase_field_of_view = Column(DECIMAL, nullable=True)
+    pixel_bandwidth = Column(INTEGER, nullable=True)
+    flip_angle = Column(DECIMAL, nullable=True)
+    rows = Column(INTEGER, nullable=True)
+    columns = Column(INTEGER, nullable=True)
+    magnetic_field_strength = Column(DECIMAL, nullable=True)
+    space_between_slices = Column(DECIMAL, nullable=True)
+    echo_train_length = Column(INTEGER, nullable=True)
+    percent_sampling = Column(DECIMAL, nullable=True)
+    pixel_spacing_0 = Column(DECIMAL, nullable=True)
+    pixel_spacing_1 = Column(DECIMAL, nullable=True)
 
 
 class Session(Base):
     __tablename__ = 'session'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    scan_id = Column(ForeignKey('scan.id'), nullable=False, index=True)
-    value = Column(String, nullable=False)
-    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
+    name = Column(VARCHAR(200), primary_key=True)
+    scan_id = Column(ForeignKey('scan.index_id'), primary_key=True)
+    date = Column(DATE)
+    index_id = Column(INTEGER, nullable=False, autoincrement=True, index=True, unique=True)
 
     scan = relationship('Scan')
-    processing_step = relationship('ProcessingStep')
+
+
+class Scan(Base):
+    __tablename__ = 'scan'
+
+    id = Column(VARCHAR(200), primary_key=True)
+    date = Column(DATE)
+    role = Column(Enum('C', 'P', 'IC', 'U', name='scan_role'))
+    comment = Column(TEXT)
+    participant_id = Column(ForeignKey('participant.index_id'), nullable=False)
+    provenance_id = Column(ForeignKey('provenance.id'), primary_key=True)
+    index_id = Column(INTEGER, nullable=False, autoincrement=True, index=True, unique=True)
+
+    participant = relationship('Participant')
+    provenance = relationship('Provenance')
 
 
 class Participant(Base):
     __tablename__ = 'participant'
 
-    id = Column(String(255), primary_key=True)
-    gender = Column(Enum('male', 'female', 'other', 'unknown', name='gender'), nullable=False)
-    handedness = Column(Enum('left', 'right', 'ambidexter', 'unknown', name='handedness'), nullable=False)
-    birthdate = Column(Date, nullable=True)
-    age = Column(Float, nullable=True)
-    processing_step_id = Column(ForeignKey('processing_step.id'), nullable=False, index=True)
+    id = Column(VARCHAR(200), primary_key=True)
+    gender = Column(Enum('male', 'female', 'other', 'unknown', name='gender'))
+    handedness = Column(Enum('left', 'right', 'ambidexter', 'unknown', name='handedness'))
+    birthdate = Column(DATE)
+    age = Column(DECIMAL)
+    provenance_id = Column(ForeignKey('provenance.id'), primary_key=True)
+    index_id = Column(INTEGER, nullable=False, autoincrement=True, index=True, unique=True)
 
-    processing_step = relationship('ProcessingStep')
+    provenance = relationship('Provenance')
