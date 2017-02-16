ba3f7960421d9644d86144de83773cec921467ea
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-16 11:52:41 +0100

    comment in test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,6 @@ echo "Starting a Postgres container..."
 postgres_container=$(docker run -d -p 5432:5432 postgres)
 sleep 5
 
-# Get gateway IP
 echo "Searching for gateway IP..."
 GATEWAY_IP=$(ip addr | grep docker | grep inet | grep -Eo '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')
 
@@ -12,6 +11,7 @@ echo "Running a migration..."
 docker run --rm -e "DB_URL=postgresql://postgres:postgres@$GATEWAY_IP:5432/postgres" hbpmip/data-catalog-setup upgrade head
 ret=$?
 
+# Remove container if not on CircleCI
 if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
     echo "Removing the Postgres container..."
     docker rm -f ${postgres_container}
